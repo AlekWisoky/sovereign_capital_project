25eebdc8e0fb8799754cf74b61162b7b60e0b7c7
Alek Wisoky <sensei7x7x7@gmail.com>
2026-09-10 05:45:06 -0700

    feat(mobile): remove legacy execution controls from primary home flow

--- mobile/src/navigation/HomeStack.tsx
+++ mobile/src/navigation/HomeStack.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeCommandScreen } from '../screens/cc/HomeCommandScreen';
-import { DashScreen } from '../screens/v2/DashScreen';
-import { TrackerScreen } from '../screens/v2/TrackerScreen';
 
 export type HomeStackParamList = {
   HomeOverview: undefined;
-  Dash: undefined;
-  Tracker: undefined;
 };
 
 const Stack = createStackNavigator<HomeStackParamList>();
@@ -16,8 +12,6 @@ export function HomeStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="HomeOverview" component={HomeCommandScreen} />
-      <Stack.Screen name="Dash" component={DashScreen} />
-      <Stack.Screen name="Tracker" component={TrackerScreen} />
     </Stack.Navigator>
   );
 }
