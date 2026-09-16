40ccf0068a2f572b7f9af463a1e09a3ea0b2b68c
Alek Wisoky <sensei7x7x7@gmail.com>
2026-09-16 21:35:05 +0100

    feat(alpha-marketplace): register marketplace API router

--- backend/victor_ai_bot/api_routes/__init__.py
+++ backend/victor_ai_bot/api_routes/__init__.py
@@ -2,6 +2,7 @@ from .admin_routes import router as admin_router
 from .advanced import router as advanced_router
 from .agents import router as agents_router
 from .analytics_routes import router as analytics_router
+from .alpha_marketplace_routes import router as alpha_marketplace_router
 from .command_center_routes import router as command_center_router
 from .engine_routes import router as engine_router
 from .evolution import router as evolution_router
@@ -36,6 +37,7 @@ __all__ = [
     "advanced_router",
     "agents_router",
     "analytics_router",
+    "alpha_marketplace_router",
     "command_center_router",
     "engine_router",
     "evolution_router",
