ceacff013797a79a5198cfc72d9637d5783b0aa1
Alek Wisoky <sensei7x7x7@gmail.com>
2026-09-24 15:49:30 -0400

    fix: lint only unused Balancer discovery indexes

--- backend/victor_ai_bot/discovery.py
+++ backend/victor_ai_bot/discovery.py
@@ -389,8 +389,8 @@ class DiscoveryManager:
             ]
             if len(supported) < 2:
                 continue
-            for pos_a, (_i, token_a) in enumerate(supported):
-                for _j, token_b in supported[pos_a + 1:]:
+            for pos_a, (i, token_a) in enumerate(supported):
+                for j, token_b in supported[pos_a + 1:]:
                     if self._curve_key(pool, i, j) not in self._curve:
                         self._curve[self._curve_key(pool, i, j)] = DiscoveredCurve(pool, token_a, token_b, i, j, int(block_number), int(block_number))
                         changed = True
@@ -435,8 +435,8 @@ class DiscoveryManager:
             ]
             if len(supported) < 2:
                 continue
-            for pos_a, (i, token_a) in enumerate(supported):
-                for j, token_b in supported[pos_a + 1:]:
+            for pos_a, (_i, token_a) in enumerate(supported):
+                for _j, token_b in supported[pos_a + 1:]:
                     if self._balancer_key(pool_id, token_a, token_b) not in self._balancer:
                         self._balancer[self._balancer_key(pool_id, token_a, token_b)] = DiscoveredBalancer(pool_id, token_a, token_b, pool, int(block_number), int(block_number))
                         changed = True
